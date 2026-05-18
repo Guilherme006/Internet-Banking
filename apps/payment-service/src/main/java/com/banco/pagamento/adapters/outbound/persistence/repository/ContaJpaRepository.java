7cfcbf740acc87a9ed6c35b3e4bc3bcd6fb94aa8
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-18 01:08:04 -0300

    Add apps/payment-service/src/main/java/com/banco/pagamento/adapters/outbound/persistence/repository/ContaJpaRepository.java

---
+++ apps/payment-service/src/main/java/com/banco/pagamento/adapters/outbound/persistence/repository/ContaJpaRepository.java
@@ -0,0 +1,19 @@
+package com.banco.pagamento.adapters.outbound.persistence.repository;
+
+import com.banco.pagamento.adapters.outbound.persistence.entity.ContaEntity;
+import jakarta.persistence.LockModeType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ContaJpaRepository extends JpaRepository<ContaEntity, Long> {
+
+        @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT c FROM ContaEntity c WHERE c.numeroConta = :numeroConta")
+    Optional<ContaEntity> findByNumeroContaWithLock(@Param("numeroConta") String numeroConta);
+}
