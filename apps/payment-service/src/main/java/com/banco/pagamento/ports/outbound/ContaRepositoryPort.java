248d188d7c2a49205fa5f9b44e8c8fdf7e4d4ecc
Guilherme006 <guilherme_lazari@outlook.com>
2026-05-18 01:08:37 -0300

    Add apps/payment-service/src/main/java/com/banco/pagamento/ports/outbound/ContaRepositoryPort.java

---
+++ apps/payment-service/src/main/java/com/banco/pagamento/ports/outbound/ContaRepositoryPort.java
@@ -0,0 +1,12 @@
+package com.banco.pagamento.ports.outbound;
+
+import com.banco.pagamento.application.domain.Conta;
+
+import java.util.Optional;
+
+public interface ContaRepositoryPort {
+
+    Optional<Conta> buscarComLockPessimista(String numeroConta);
+
+    Conta salvar(Conta conta);
+}
